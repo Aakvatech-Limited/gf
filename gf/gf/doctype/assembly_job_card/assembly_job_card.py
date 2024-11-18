f407885043c8eab3e9500fc5fc2397f9d357a1fa
aakvatech <info@aakvatech.com>
2024-11-18 10:35:35 +0300

    feat: set working hours

--- gf/gf/doctype/assembly_job_card/assembly_job_card.py
+++ gf/gf/doctype/assembly_job_card/assembly_job_card.py
@@ -8,11 +8,49 @@ from frappe.model.document import Document
 class AssemblyJobCard(Document):
 	def before_save(self):
 		self.set_working_hours()
+		self.add_sickbay_task()
+	
+	def before_submit(self):
+		self.validate_submit_status()
+
+		self.status = "Completed"
 
 	def set_working_hours(self):
-		for row in self.job_card_detail:
-			if row.start_datetime and row.pause_datetime:
-				row.time_elapsed = time_diff_in_hours(row.start_datetime, row.pause_datetime)
+		self.assembly_total_hours = 0
+		self.cabinet_total_hours = 0
+		self.bodyshop_total_hours = 0
+		self.sickbay_total_hours = 0
+		
+		for row in self.assembly_job_detail:
+			if row.start_datetime and row.pending_datetime and not row.end_datetime:
+				row.total_time_elapsed = time_diff_in_hours(row.pending_datetime, row.start_datetime)
+				self.assembly_total_hours += row.total_time_elapsed
 			
 			if row.start_datetime and row.end_datetime:
-				row.time_elapsed = time_diff_in_hours(row.start_datetime, row.end_datetime)
+				row.total_time_elapsed = time_diff_in_hours(row.end_datetime, row.start_datetime)
+				self.assembly_total_hours += row.total_time_elapsed
+		
+		for row in self.cabinet_job_detail:
+			if row.start_datetime and row.pending_datetime and not row.end_datetime:
+				row.total_time_elapsed = time_diff_in_hours(row.pending_datetime, row.start_datetime)
+				self.cabinet_total_hours += row.total_time_elapsed
+			
+			if row.start_datetime and row.end_datetime:
+				row.total_time_elapsed = time_diff_in_hours(row.end_datetime, row.start_datetime)
+				self.cabinet_total_hours += row.total_time_elapsed
+		
+		for row in self.bodyshop_job_detail:
+			if row.start_datetime and row.pending_datetime and not row.end_datetime:
+				row.total_time_elapsed = time_diff_in_hours(row.pending_datetime, row.start_datetime)
+				self.bodyshop_total_hours += row.total_time_elapsed
+			
+			if row.start_datetime and row.end_datetime:
+				row.total_time_elapsed = time_diff_in_hours(row.end_datetime, row.start_datetime)
+				self.bodyshop_total_hours += row.total_time_elapsed
+		
+		for row in self.sickbay_job_detail:
+			if row.start_datetime and row.end_datetime:
+				row.total_time_elapsed = time_diff_in_hours(row.end_datetime, row.start_datetime)
+				self.sickbay_total_hours += row.total_time_elapsed
+
+	
